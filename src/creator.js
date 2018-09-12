fe18690221873afd2facf07f5fed95501c6453e9
Lespagnol Rémy <remy.lespagnol@gmail.com>
2018-09-12 13:32:01 +0200

    :bug: fix bug with case sensitive system

--- src/creator.js
+++ src/creator.js
@@ -6,7 +6,7 @@ const {
     migratesWorkspaces, 
     listAllWorkspacesNames,
 	  dumpWorkspaces
-} = require('IWAC-utils');
+} = require('iwac-utils');
 
 const questions = require('./config/questions');
 const workspaces = require('./config/wks');
